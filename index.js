514547210ad31bc454ce213580744eca7c24ef6b
farhanakhanswe <farhanakhanswe@gmail.com>
2023-10-14 22:17:14 +0600

    using localStorage now to store the links

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ let trackedLinks = [];
 
 saveLinkBtn.addEventListener("click", () => {
     trackedLinks.push(linkInputEl.value);
+    localStorage.setItem("trackedLinks", JSON.stringify(trackedLinks));
     renderLinks();
 });
 
 const renderLinks = () => {
     let listItems = "";
+
     for (let i = 0; i < trackedLinks.length; i++) {
         listItems += `<li><a target="_blank" href="${trackedLinks[i]}">${trackedLinks[i]}</a></li>`;
     }
+
     linksList.innerHTML = listItems;
     linkInputEl.value = "";
 }
