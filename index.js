8e52a0b84ebaf9ec3cda45b1f4aace85d6a2c882
farhanakhanswe <farhanakhanswe@gmail.com>
2023-10-16 18:50:14 +0600

    automatically saving current tab feature implemented

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
 const linkInputEl = document.getElementById("link-input");
 const saveLinkBtn = document.getElementById("save-link-btn");
+const saveCurrentTabBtn = document.getElementById("save-current-tab-btn");
 const deleteAllLinksBtn = document.getElementById("delete-all-links-btn");
 const linksList = document.getElementById("links-list");
-let trackedLinks = [];
 const linksFromLocalStorage = JSON.parse(localStorage.getItem("trackedLinks"));
+let trackedLinks = [];
+
+const renderLinks = (links) => {
+    try {
+        let listItems = "";
+        for (let i = 0; i < links.length; i++) {
+            listItems += `<li><a target="_blank" href="${links[i]}">${links[i]}</a></li>`;
+        }
+        linksList.innerHTML = listItems;
+        linkInputEl.value = "";
+    } catch (err) {
+        alert("Something went wrong!");
+    }
+
+}
+
+const setLocalStorageItem = (array) => {
+    try {
+        localStorage.setItem("trackedLinks", JSON.stringify(array));
+    } catch (err) {
+        alert("Something went wrong!");
+    }
+}
 
 if (linksFromLocalStorage) {
-    trackedLinks = linksFromLocalStorage;
-    renderLinks();
+    renderLinks(linksFromLocalStorage);
 }
 
 saveLinkBtn.addEventListener("click", () => {
-    trackedLinks.push(linkInputEl.value);
-    localStorage.setItem("trackedLinks", JSON.stringify(trackedLinks));
-    renderLinks();
+    if (linkInputEl.value === "") return alert("Please enter a link!");
+
+    try {
+        trackedLinks.push(linkInputEl.value);
+        setLocalStorageItem(trackedLinks)
+        renderLinks(trackedLinks);
+    } catch (err) {
+        alert("Something went wrong!");
+    }
 });
 
-deleteAllLinksBtn.addEventListener("click", () => {
-    localStorage.clear();
-    trackedLinks = [];
-    renderLinks();
+saveCurrentTabBtn.addEventListener("click", () => {
+    try {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            console.log(tabs);
+        });
+        setLocalStorageItem(trackedLinks)
+        renderLinks(trackedLinks);
+    } catch (err) {
+        alert("Something went wrong!");
+    }
+
 })
 
-const renderLinks = () => {
-    let listItems = "";
-    for (let i = 0; i < trackedLinks.length; i++) {
-        listItems += `<li><a target="_blank" href="${trackedLinks[i]}">${trackedLinks[i]}</a></li>`;
+deleteAllLinksBtn.addEventListener("click", () => {
+    try {
+        localStorage.clear();
+        trackedLinks = [];
+        renderLinks(trackedLinks);
+    } catch (err) {
+        alert("Something went wrong!");
     }
-    linksList.innerHTML = listItems;
-    linkInputEl.value = "";
-}
+})
 
 
 
