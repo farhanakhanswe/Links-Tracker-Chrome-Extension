dba779a1594f5adda144d1c4335083a88b674c66
farhanakhanswe <farhanakhanswe@gmail.com>
2023-10-16 10:45:00 +0600

    tracked links from local storage now rendering on view

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const linkInputEl = document.getElementById("link-input");
 const saveLinkBtn = document.getElementById("save-link-btn");
 const linksList = document.getElementById("links-list");
 let trackedLinks = [];
+let linksFromLocalStorage = JSON.parse(localStorage.getItem("trackedLinks"));
+
+const renderLinks = () => {
+    let listItems = "";
+    for (let i = 0; i < trackedLinks.length; i++) {
+        listItems += `<li><a target="_blank" href="${trackedLinks[i]}">${trackedLinks[i]}</a></li>`;
+    }
+    linksList.innerHTML = listItems;
+    linkInputEl.value = "";
+}
+
+if (linksFromLocalStorage) {
+    trackedLinks = linksFromLocalStorage;
+    renderLinks();
+}
 
 saveLinkBtn.addEventListener("click", () => {
     trackedLinks.push(linkInputEl.value);
@@ -9,16 +24,6 @@ saveLinkBtn.addEventListener("click", () => {
     renderLinks();
 });
 
-const renderLinks = () => {
-    let listItems = "";
-
-    for (let i = 0; i < trackedLinks.length; i++) {
-        listItems += `<li><a target="_blank" href="${trackedLinks[i]}">${trackedLinks[i]}</a></li>`;
-    }
-
-    linksList.innerHTML = listItems;
-    linkInputEl.value = "";
-}
 
 
 
